ac7e7f562e2f2839ba47377a86e05500f073341b
FANM5 <fabricenolla@aol.co.uk>
2023-03-14 07:27:16 +0000

    Adding Cloudwatch to app py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,6 +36,20 @@ provider.add_span_processor(processor)
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
+#cloudwatch Logs ------------------------------------------------------------------------
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
 
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -74,6 +88,12 @@ def data_message_groups():
     return model['errors'], 422
   else:
     return model['data'], 200
+    
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
