5ea9a18c1300374a78ed3dbb6d87b25427223e4e
FANM5 <fabricenolla@aol.co.uk>
2023-03-14 20:45:52 +0000

    re-enabling cloudwatch so the backend can run

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -49,13 +49,13 @@ from flask import got_request_exception
 
 
 # Configuring Logger to Use CloudWatch
-#LOGGER = logging.getLogger(__name__)
-#LOGGER.setLevel(logging.DEBUG)
-#console_handler = logging.StreamHandler()
-#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-#LOGGER.addHandler(console_handler)
-#LOGGER.addHandler(cw_handler)
-#LOGGER.info("test log")
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
 
 
 # Show this in the logs within the backend-flask app (STDOUT)
@@ -114,11 +114,11 @@ def data_message_groups():
   else:
     return model['data'], 200
     
-#@app.after_request
-#def after_request(response):
-    #timestamp = strftime('[%Y-%b-%d %H:%M]')
-    #LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    #return response
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route('/rollbar/test')
 def rollbar_test():
