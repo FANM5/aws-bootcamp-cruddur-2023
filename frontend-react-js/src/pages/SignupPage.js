ef1fe368f7fc56c4fcf0d35bb00d92538994d1f5
FANM5 <fabricenolla@aol.co.uk>
2023-03-18 22:15:35 +0000

    signup page update attributes

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -24,6 +24,7 @@ export default function SignupPage() {
         password: password,
         attributes: {
           name: name,
+          email: username,
           preferred_username: username,
         },
         autoSignIn: { // optional - enables auto sign in after user is confirmed
